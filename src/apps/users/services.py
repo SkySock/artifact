1a86b475dbcd65369cd7b60ff8dffd3d1b532c83
SkySock <inikitenko@list.ru>
2022-06-15 22:52:37 +0500

    add paginated_response_schema

--- src/apps/users/services.py
+++ src/apps/users/services.py
@@ -9,10 +9,36 @@ class PaginationUsers(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link(),
-            },
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
             'count': self.page.paginator.count,
             'users': data,
         })
+
+    def get_paginated_response_schema(self, schema):
+        return {
+            'type': 'object',
+            'properties': {
+                'next': {
+                    'type': 'string',
+                    'nullable': True,
+                    'format': 'uri',
+                    'example': 'http://api.example.org/accounts/?{page_query_param}=4&{page_size_query_param}=30'.format(
+                      page_query_param=self.page_query_param, page_size_query_param=self.page_size_query_param
+                    )
+                },
+                'previous': {
+                    'type': 'string',
+                    'nullable': True,
+                    'format': 'uri',
+                    'example': 'http://api.example.org/accounts/?{page_query_param}=2&{page_size_query_param}=30'.format(
+                        page_query_param=self.page_query_param, page_size_query_param=self.page_size_query_param
+                    )
+                },
+                'count': {
+                    'type': 'integer',
+                    'example': 123,
+                },
+                'users': schema,
+            }
+        }
