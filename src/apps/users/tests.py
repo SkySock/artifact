7ddc5298ff1bd66f4f696a5f8775e244443ef327
SkySock <inikitenko@list.ru>
2022-07-27 17:29:48 +0500

    add follow test

--- src/apps/users/tests.py
+++ src/apps/users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, force_authenticate
+from .models import ArtifactUser, UserFollowing
 
-# Create your tests here.
+
+class FollowTest(APITestCase):
+    def setUp(self):
+        ArtifactUser.objects.create(username='TestUser_1')
+        ArtifactUser.objects.create(username='TestUser_2')
+
+    def test_follow(self):
+        user_1 = ArtifactUser.objects.get(username='TestUser_1')
+        user_2 = ArtifactUser.objects.get(username='TestUser_2')
+        self.client.force_authenticate(user=user_1)
+        request = self.client.post(f'/api/v1/users/follow/{user_2.pk}/')
+
+        self.assertEqual(request.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(request.data, {'is_followed': True})
+        self.assertEqual(UserFollowing.objects.count(), 1)
+        self.assertEqual(UserFollowing.objects.get().user, user_1)
+        self.assertEqual(UserFollowing.objects.get().following_user, user_2)
